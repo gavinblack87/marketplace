dd6ed644d691c655376ad1995563101fcb7d16f6
gavinblack87 <gavin_black@hotmail.co.uk>
2019-12-03 14:45:30 +0000

    seed data

--- IgsApi/Controllers/IgsController.cs
+++ IgsApi/Controllers/IgsController.cs
@@ -1,5 +1,7 @@
 using System.Collections.Generic;
 using Microsoft.AspNetCore.Mvc;
+using IgsApi.Models;
+using Microsoft.EntityFrameworkCore;
 
 namespace IgsApi.Controllers
 {
@@ -7,10 +9,72 @@ namespace IgsApi.Controllers
     [ApiController]
     public class IgsController : ControllerBase
     {
+        private readonly ItemContext _context;
+
+        public IgsController(ItemContext context) => _context = context;
+    
+        //GET: api/items
         [HttpGet]
-        public ActionResult<IEnumerable<string>> GetString()
+        public ActionResult<IEnumerable<Item>> GetItems()
         {
-            return new string[] {"this", "is", "hard", "coded"};
+            return _context.Items;
+        }
+
+        //GET:      api/items/n
+        [HttpGet("{id}")]
+        
+        public ActionResult<Item> GetItems(int id)
+        {
+            var itemsItem = _context.Items.Find(id);
+
+            if (itemsItem == null)
+            {
+                return NotFound();
+            }
+
+            return itemsItem;
+        }
+        
+        //POST:     api/items
+        [HttpPost]
+        public ActionResult<Item> PostItem(Item item)
+        {
+            _context.Items.Add(item);
+            _context.SaveChanges();
+
+            return CreatedAtAction("GetItem", new Item{Id = item.Id}, item);
+        }
+
+        //PUT:      api/items/n
+        [HttpPut("{id}")]
+        public ActionResult PutItem(int id, Item item)
+        {
+            if (id != item.Id)
+            {
+                return BadRequest();
+            }
+
+            _context.Entry(item).State = EntityState.Modified;
+            _context.SaveChanges();
+
+            return NoContent();
+        }
+
+        //DELETE:   api/commands/n
+        [HttpDelete("{id}")]
+        public ActionResult<Item> DeleteItem(int id)
+        {
+            var item = _context.Items.Find(id);
+
+            if (item == null)
+            {
+                return NotFound();
+            }
+
+            _context.Items.Remove(item);
+            _context.SaveChanges();
+
+            return item;
         }
     }
-}
+    }
--- IgsApi/models/ItemContext.cs
+++ IgsApi/models/ItemContext.cs
@@ -14,7 +14,31 @@ namespace IgsApi.Models
 
         }
         
-        public DbSet<Item> ItemItems {get; set;}
+        public DbSet<Item> Items {get; set;}
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Item>().HasData(
+                new Item
+                {
+                    Id = 001,
+                    Name = "Lavender Heart",
+                    Price = 9.25
+                },
+                new Item
+                {
+                    Id = 002,
+                    Name = "Personalised cufflinks",
+                    Price = 45
+                },
+                new Item
+                {
+                    Id = 003,
+                    Name = "Kids T-shirt",
+                    Price = 19.95
+                }
+            );
+        }
 
     }
 }
