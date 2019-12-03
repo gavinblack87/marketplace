4ae243654370aeec93fd873cbc2d3450a6da3121
gavinblack87 <gavin_black@hotmail.co.uk>
2019-12-03 16:33:48 +0000

    docker edit

--- IgsApi/Startup.cs
+++ IgsApi/Startup.cs
@@ -22,8 +22,6 @@ namespace IgsApi
 
         public Startup(IConfiguration configuration) => Configuration = configuration;
 
-        // This method gets called by the runtime. Use this method to add services to the container.
-        // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddDbContext<ItemContext>
@@ -41,7 +39,6 @@ namespace IgsApi
             });
         }
 
-        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
             app.UseMvc();
